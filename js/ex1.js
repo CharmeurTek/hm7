2f4bbc2306ecf8d82fe72e3d855173c99ebbb16a
Hugo Lalet <hugo@MacBook-Pro-de-Hugo.local>
2022-04-06 22:14:48 -0700

    ex1 and 2 done

---
+++ js/ex1.js
@@ -0,0 +1,69 @@
+// Character list. Each house has a name and a code
+const houses = [
+    {
+      code: "ST",
+      name: "Stark"
+    },
+    {
+      code: "LA",
+      name: "Lannister"
+    },
+    {
+      code: "BA",
+      name: "Baratheon"
+    },
+    {
+      code: "TA",
+      name: "Targaryen"
+    }
+  ];
+  
+  // Return an array of characters belonging to a house
+  const getCharacters = houseCode => {
+    switch (houseCode) {
+      case "ST":
+        return ["Eddard", "Catelyn", "Robb", "Sansa", "Arya", "Jon Snow"];
+      case "LA":
+        return ["Tywin", "Cersei", "Jaime", "Tyrion"];
+      case "BA":
+        return ["Robert", "Stannis", "Renly"];
+      case "TA":
+        return ["Aerys", "Daenerys", "Viserys"];
+      default:
+        return []; // Empty array
+    }
+  };
+
+  const addValueOnSelect = () => {
+      for (house of houses) {
+        const elements = document.getElementById('house');
+        var options = document.createElement('option');
+        options.appendChild(document.createTextNode(house.name));
+        options.value = house.code;
+        elements.appendChild(options);
+      }
+  }
+
+const displayCharacters = () => {
+  for(character of getCharacters(houseCode=valueHouse)) {
+    const elements = document.getElementById('characters');
+    var thecharacter = document.createElement('li');
+    thecharacter.text = "hugo";
+    elements.appendChild(thecharacter);
+  }
+}
+
+addValueOnSelect();
+
+document.getElementById("house").addEventListener("change", e => {
+    const Child = document.getElementById("characters");
+    while (Child.firstChild) {
+        Child.removeChild(Child.lastChild);
+    }
+    for(character of getCharacters(houseCode=e.target.value)) {
+        const elements = document.getElementById('characters');
+        var thecharacter = document.createElement('li');
+        thecharacter.appendChild(document.createTextNode(character));
+        elements.appendChild(thecharacter);
+      }
+  });
---
+++ js/ex2.js
@@ -0,0 +1,45 @@
+// Country list
+const countryList = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antarctica",
+    "Antigua-and-Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Autria",
+    "Azerbaïjan"
+  ];
+
+    // function capitalize(str)
+    // {
+    //     return str.charAt(0).toUpperCase() + str.substr(1);
+    // }
+
+    // const input = document.getElementById('country');
+    // const result = document.getElementById('suggestions');
+    //     input.addEventListener('input', e =>  {
+    //         var reg = new RegExp(capitalize(e.target.value));
+    //         var res = countryList.filter(function(term) {
+    //             if (term.match(reg)) {
+    //                 return term;
+    //             }
+    //         });
+    //         const Child = document.getElementById("suggestions");
+    //         while (Child.firstChild) {
+    //             Child.removeChild(Child.lastChild);
+    //         }
+    //         if (e.target.selectionEnd <= 0)
+    //             return
+    //         for(country of res) {
+    //             const elements = document.getElementById('suggestions');
+    //             var countries = document.createElement('p');
+    //             countries.appendChild(document.createTextNode(country));
+    //             elements.appendChild(countries);
+    //         }
+    //     });
